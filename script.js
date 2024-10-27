d4c6c624422ce0882501765856ab6562b6715f54
candrews6 <charlie.andrews6@mail.com>
2024-10-27 15:39:45 +0000

    add responsiveness for menus

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById("hamburger").addEventListener("click", function() {
     const menu = document.querySelector(".nav-menu");
     menu.classList.toggle("active");
+    this.classList.toggle("active"); // Toggle active class on hamburger
 });
